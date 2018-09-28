bb45fe6addb898ade4a922f939712ff1e189b63b
Manish Aneja <manishekaneja@gmail.com>
2018-09-28 18:34:25 +0530

    Frontend Layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, AppBar, Toolbar, Typography, Grid, Paper } from "@material-ui/core";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<React.Fragment>
+      <CssBaseline />
+      <AppBar color="primary">
+        <Toolbar>
+          <Typography variant="headline" color="inherit">
+            PosLearn
+        </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={24}>
+        <Grid item md={6} xs={12}>
+          <Paper>
+            <img src alt={"First"} style={{ width: '100 %' }} />
+          </Paper>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Paper>
+            <img src alt={"second"} style={{ width: '100 %' }} />
+          </Paper>
+        </Grid>
+      </Grid>
+
+    </React.Fragment>);
   }
 }
 
