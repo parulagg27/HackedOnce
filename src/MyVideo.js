b432d130fe83e47ff88352ba6941dc53e27ffec7
Manish Aneja <manishekaneja@gmail.com>
2018-09-29 01:18:44 +0530

    Display Video

--- src/MyVideo.js
+++ src/MyVideo.js
@@ -18,14 +18,25 @@ class MyVideo extends React.Component {
                 },
             };
             obj.video.deviceId = devi.deviceId;
+            navigator.mediaDevices.getUserMedia(obj).then((dd) => {
+                let track = dd.getVideoTracks()[0];
+                console.log(track)
+                this.abc = track;
+                this.zozo = window.URL.createObjectURL(dd);
+                console.log(this.zozo)
+                document.querySelector('video').src = track
+                this.cc = document.createElement('canvas');
+                const context = this.cc.getContext('2d');
+                document.querySelector('#can').getContext('2d').drawImage(document.querySelector('video'), 0, 0);
+            });
         });
     }
 
 
     render = () => <React.Fragment>
         <Paper>
-            <canvas id="can" width="0" height="0" style={{ border: "1px solid black", display: "hidden" }}></canvas>
-            <video src="" style={{ left: "0px", top: "0px", margin: "0px", backgroundColor: "black", width: "100%", height: "100%", border: "2px solid black", position: "absolute", zIndex: "4" }} ></video>
+            <canvas id="can" width="0" height="0" style={{ border: "1px solid black", right: "0px", top: "0px", margin: "0px", backgroundColor: "pink", width: "50%", height: "100%", border: "2px solid black", position: "absolute", zIndex: "4" }}></canvas>
+            <video src="" style={{ left: "0px", top: "0px", margin: "0px", backgroundColor: "pink", width: "50%", height: "100%", border: "2px solid black", position: "absolute", zIndex: "4" }} ></video>
         </Paper>
     </React.Fragment>
 }
